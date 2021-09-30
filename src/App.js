7bcab1ce943404609e3e7eeef0119f3030f479f8
abu-nofal <anofal719@gmail.com>
2021-09-30 11:47:09 +0300

    setup the front project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import {
+//     BrowserRouter ,
+//     Switch,
+//     Route,
+//     Link
+//   } from "react-router-dom";
+function App() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom'
+import App from './App'
+
+
+function Main (){
+    return(<App/>)
+}
+ReactDom.render(<Main/>,document.getElementById('root'))
