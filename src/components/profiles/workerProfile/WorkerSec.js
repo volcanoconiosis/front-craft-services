0c0fa83c3c0466f5b6983508fe62c667a0f35499
abu-nofal <anofal719@gmail.com>
2021-10-03 21:26:06 +0300

    add ViewWorkerPage

---
+++ src/components/profiles/workerProfile/WorkerSec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WorkerSec() {
+    return (
+        <div>
+            <h2>Under Processing ... 🔴🟡🟢</h2>
+        </div>
+    )
+}
+
+export default WorkerSec
